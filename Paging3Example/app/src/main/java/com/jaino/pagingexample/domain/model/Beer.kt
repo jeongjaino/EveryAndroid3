908d850db03303b357f480d4abdc37ac6357abda
jeongjaino <akgk155@gmail.com>
2023-07-24 14:20:16 +0900

    Paging3Example : RemoteMediator 및 Beer Model 구현

---
+++ Paging3Example/app/src/main/java/com/jaino/pagingexample/domain/model/Beer.kt
@@ -0,0 +1,10 @@
+package com.jaino.pagingexample.domain.model
+
+data class Beer(
+    val id: Int,
+    val name: String,
+    val tagline: String,
+    val description: String,
+    val first_brewed: String,
+    val image_url: String?
+)
