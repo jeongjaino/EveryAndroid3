3a63bb2f9c3856a625d0402e1ee459ca779295f4
jeongjaino <akgk155@gmail.com>
2023-08-30 15:58:03 +0900

    Finish Testing Course 3-4

--- TestingCourse/TestingCourse3/app/src/test/java/com/plcoding/testingcourse/shopping/domain/ShoppingCartTest.kt
+++ TestingCourse/TestingCourse3/app/src/test/java/com/plcoding/testingcourse/shopping/domain/ShoppingCartTest.kt
@@ -6,6 +6,9 @@ import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class ShoppingCartTest{
 
@@ -30,7 +33,6 @@ internal class ShoppingCartTest{
             name = "아이스크림",
             price = 40.0
         )
-
         shoppingCart.addProduct(product, 4)
 
         // ACTION
@@ -71,5 +73,53 @@ internal class ShoppingCartTest{
         assertFailure {
             shoppingCart.addProduct(product, -4)
         }
+    }
+
+    // ParameterizedTest
+    // 매개변수로 들어오는 값을 설정할 수 있다.
+    @ParameterizedTest
+    @ValueSource(
+        ints = [1, 2, 3, 4, 5]
+    )
+    fun `여러 개의 상품을 1 ~ 5 번 더했을 때, 매번 총 가격이 일치해야 한다`(quantity: Int){
+        // GIVEN
+        val product = Product(
+            id = 1,
+            name = "아이스크림",
+            price = 40.0
+        )
+        shoppingCart.addProduct(product, quantity)
+
+        // ACTION
+        val totalCost = shoppingCart.getTotalCost()
+
+        // ASSERTION
+        assertThat(totalCost).isEqualTo(40.0 * quantity)
+    }
+
+    // 매개변수가 2개 이상일 때, Csv타입으로 설정할 수 있다.
+    @ParameterizedTest
+    @CsvSource(
+        "4,20.0",
+        "3,15.0",
+        "6,30.0"
+    )
+    fun `여러 개의 상품을 quantity번 더했을 때, 매번 총 가격이 expectedPriceSum과 일치해야 한다`(
+        quantity: Int,
+        expectedPriceSum: Double
+    ){
+        // GIVEN
+        val product = Product(
+            id = 1,
+            name = "아이스크림",
+            price = 5.0
+        )
+        shoppingCart.addProduct(product, quantity)
+
+        // ACTION
+        val totalCost = shoppingCart.getTotalCost()
+
+        // ASSERTION
+        assertThat(totalCost).isEqualTo(expectedPriceSum)
     }
 }
